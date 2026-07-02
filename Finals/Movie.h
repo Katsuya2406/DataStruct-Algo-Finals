b719a6368cd0abe594a953039a0e812563f5cfb4
RC Programming <reigepayad24@gmail.com>
2026-07-02 12:54:44 +0800

    Update Movie class with new methods and fix rating
    Refactored Movie class to include additional methods for managing movies.

--- Finals/Movie.h
+++ Finals/Movie.h
@@ -1,23 +1,18 @@
-#ifndef MRENTAL_H
-#define MRENTAL_H
-#include <string>
+class Movie {
+private:
+    string title;
+    string genre;
+    string rating;
+    int year;
 
-using namespace std;
-class Movie{
-	private:
-		string title;
-		string genre;
-		string rate;
-		int year;
-		
-	public:
-		Movie();
-		Movie(string t, string g, string r, int yr);
-		
-		void setTitle(string t);
-		void setGenre(string g);
-		void setRating(string r)l
-		void setYear(int yr);
-		
-		void addTitle (const string& )
+public:
+    Movie();
+    Movie(string t, string g, string r, int yr);
+
+    void addMovie(string t, string g, string r, int yr);
+    void deleteMovie(string title);
+    void searchMovie(string title);
+    void saveToFile(string filename);
+    void loadFromFile(string filename);
+    void displayMenu();
 };
