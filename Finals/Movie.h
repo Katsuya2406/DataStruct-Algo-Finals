59fdf851c424635ee830b012549fcd5b32b70584
jjssimon <jenjohsimon@gmail.com>
2026-07-16 18:14:08 +0800

    Update Movie.h
    Finalized with linked list and other features.

--- Finals/Movie.h
+++ Finals/Movie.h
@@ -1,30 +1,64 @@
 #ifndef MOVIE_H
 #define MOVIE_H
 
-#include <queue>
+#include <iostream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
+//movie as a class in-order to list down the data members of title, year, and publisher. 
+class Movie{
+//by encapsulation, private data members and the pointer for the linked list.
+private:
+	//data members that will store the value later given by the user.
+    string movieTitle;
+    int movieYear;
+    string moviePublisher;
+    
+    //for the next movie in the list.
+    Movie* next;
 
-//Queue Functions:
+public:
+    //constructor (no parameter) default value.
+    Movie();
+    Movie(string title, int year, string publisher);
+
+    //setters, access the private data members.
+    void setMovieTitle(string title);
+    void setMovieYear(int year);
+    void setMoviePublisher(string publisher);
+    void setNext(Movie* n);
+
+    //getters, modifies the value of private data members.
+    string getMovieTitle() const;
+    int getMovieYear() const;
+    string getMoviePublisher() const;
+    Movie* getNext() const;
+};
+
+//functions for loading and saving the linked list of movies to a file.
+void loadMoviesFromFile(Movie*& head, const string& filename);
+void saveMoviesToFile(Movie* head, const string& filename);
+
+//functions for managing the movie linked list.
+void addMovie(Movie*& head);
+void deleteMovie(Movie*& head);
+void searchMovie(Movie* head);
+void displayMovies(Movie* head);
+void viewNextMovie(Movie* head);
+void checkIfEmpty(Movie* head);
+
+//functions for loading and saving the customer queue.
 void loadQueueFromFile(queue<string>& q, const string& filename);
 void saveQueueToFile(const queue<string>& q, const string& filename);
 
-//Main Functions:
-void addMovie(queue<string>& movieQueue);
-void rentMovie(queue<string>& movieQueue, queue<string>& customerQueue);
-void viewNextMovie(const queue<string>& movieQueue);
-void checkIfEmpty(const queue<string>& movieQueue);
-
-//Extra Functions
-void searchMovie();
-void displayMovie();
+//functions for renting and returning movies using the customer queue.
+void rentMovie(Movie*& head, queue<string>& customerQueue);
+void returnMovie(Movie*& head, queue<string>& customerQueue);
 void displayCustomerQueue(const queue<string>& customerQueue);
-void returnMovie(queue<string>& movieQueue, queue<string>& customerQueue);
-void deleteMovie(queue<string>& movieQueue);
 
-//Menu
+//displays the main menu.
 void displayMenu();
 
 #endif
